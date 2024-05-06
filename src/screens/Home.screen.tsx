25484d73f01b26018243898821bc1d62840a15d7
Fraser Ross <44373044+fraser182@users.noreply.github.com>
2024-05-06 16:54:22 +0100

    Removed commented out code - All components into AssemblyLine

--- src/screens/Home.screen.tsx
+++ src/screens/Home.screen.tsx
@@ -2,41 +2,14 @@ import React, { useState } from 'react';
 import { View, TextInput, Button, StyleSheet, SafeAreaView } from 'react-native';
 import { AssemblyLine } from '../components/AssemblyLine.component';
 
-// import { AssemblyLine } from '../components/AssemblyLine.component';
 
 export const HomeScreen = () => {
-  // const [inputText, setInputText] = useState('');
-  // const [items, setItems] = useState({ idea: [], development: [], testing: [], deployment: [] });
-
-  // const handleInputChange = (text) => {
-  //   setInputText(text);
-  // };
-
-  // const handleButtonPress = () => {
-  //   const stage = 'idea'; // Default to 'idea' for now
-  //   setItems((prevItems) => ({ ...prevItems, [stage]: [...prevItems[stage], inputText] }));
-  //   setInputText('');
-  // };
+ 
 
   return (
-    <SafeAreaView style={styles.container}>
-      {/* <TextInput
-        style={styles.input}
-        onChangeText={handleInputChange}
-        value={inputText}
-        placeholder="Enter item"
-      />
-      <Button title="Add Item" onPress={() => handleButtonPress} /> */}
+    <SafeAreaView>
+
       <AssemblyLine stages={["Idea", "Development", "Testing", "Deployment"]} />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // paddingHorizontal: 20,
-  },
-});
